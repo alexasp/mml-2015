1461d7c2be0d9f230b6e93a60814f7e37b1a5fcb
unknown <peterfh@stud2103.idipc.idi.ntnu.no>
2015-02-24 16:24:26 +0100

    testing

--- source/PreliminaryExperiments/PerformanceMetrics.cs
+++ source/PreliminaryExperiments/PerformanceMetrics.cs
@@ -6,6 +6,12 @@ namespace PreliminaryExperiments
     {
         public static void ErrorRate(List<double?> labels, Dataset testSet)
         {
+            foreach (var label in labels)
+            {
+                var i = 0;
+                testSet.Samples.
+            }
+
             throw new System.NotImplementedException();
         }
     }
