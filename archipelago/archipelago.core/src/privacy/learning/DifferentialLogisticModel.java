f0102c97b827f648f793aa8ff1f9429a27360fb2
Alexander Asplund <alexasplund@gmail.com>
2015-03-09 13:05:34 +0100

    Made DifferentialPrivacyModel implement Model

--- archipelago/archipelago.core/src/privacy/learning/DifferentialLogisticModel.java
+++ archipelago/archipelago.core/src/privacy/learning/DifferentialLogisticModel.java
@@ -1,11 +1,12 @@
 package privacy.learning;
 
+import learning.Model;
 import privacy.NoisyQueryable;
 
 /**
  * Created by alex on 3/5/15.
  */
-public class DifferentialLogisticModel {
+public class DifferentialLogisticModel implements Model {
 
 
     public void step(double epsilon, NoisyQueryable queryable) {
