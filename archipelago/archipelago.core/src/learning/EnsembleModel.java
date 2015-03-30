707fdad3d4fe3a3042e82a892783ceca512322a8
Alexander Asplund <alexasplund@gmail.com>
2015-03-30 17:14:24 +0200

    Agents no longer die

--- archipelago/archipelago.core/src/learning/EnsembleModel.java
+++ archipelago/archipelago.core/src/learning/EnsembleModel.java
@@ -2,12 +2,21 @@ package learning;
 
 import privacy.NoisyQueryable;
 
+import java.util.ArrayList;
+
 /**
  * Created by alex on 3/9/15.
  */
 public class EnsembleModel implements Model{
+
+    private final ArrayList<Model> _ensemble;
+
+    public EnsembleModel() {
+        _ensemble = new ArrayList<>();
+    }
+
     public void add(Model model) {
-        throw new UnsupportedOperationException("Not yet implemented.");
+        _ensemble.add(model);
     }
 
     @Override
