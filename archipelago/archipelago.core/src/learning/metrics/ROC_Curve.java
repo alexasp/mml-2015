ff7356b940c1726b2aa557e128e4694d3e645d4b
Peter Froystad <p.froystad@gmail.com>
2015-05-05 12:10:51 +0200

    Added best confusion matrix to each Roc-curve sheet

--- archipelago/archipelago.core/src/learning/metrics/ROC_Curve.java
+++ archipelago/archipelago.core/src/learning/metrics/ROC_Curve.java
@@ -25,11 +25,10 @@ public class ROC_Curve {
     String _experimentName;
 
 
-    public ROC_Curve(String path,String experimentName)
+    public ROC_Curve(String path)
     {
         _path = path;
         workbook = new HSSFWorkbook();
-        _experimentName = experimentName;
     }
 
     public void add(List<ConfusionMatrix> matrices, String peerName){
@@ -46,8 +45,10 @@ public class ROC_Curve {
     public void formChartObject(List<ConfusionMatrix> listOfConfusionMatrices){
         //Create a new row in current sheet
         int rownum = 1;
-        double maxClassification = 0.0d;
-        double maxClassificationThreshold = 0.0d;
+        double maxClassification = listOfConfusionMatrices.get(0).getCorrectClassifiedPercentage();
+        double maxClassificationThreshold = listOfConfusionMatrices.get(0).getThreshold();
+
+        ConfusionMatrix _bestConfusionMatrix = listOfConfusionMatrices.get(0);
 
         for(ConfusionMatrix matrix : listOfConfusionMatrices) {
             HSSFRow row = sheet.createRow(rownum++);
@@ -59,13 +60,40 @@ public class ROC_Curve {
             HSSFCell cell3 = row.createCell(2);
             cell3.setCellValue(matrix.getThreshold());
 
-//            if(matrix.getCorrectClassifiedPercentage()>maxClassification){
-//                maxClassification=matrix.getCorrectClassifiedPercentage();
-//                maxClassificationThreshold = matrix.getThreshold();
-//            }
+            if(matrix.getCorrectClassifiedPercentage()>maxClassification){
+                maxClassification=matrix.getCorrectClassifiedPercentage();
+                maxClassificationThreshold = matrix.getThreshold();
+                _bestConfusionMatrix=matrix;
+            }
         }
-    }
-    public void getBestConfusionMatrix(){
+        HSSFRow jumpRow = sheet.createRow(rownum+2);
+        HSSFCell cell1 =jumpRow.createCell(0);
+        cell1.setCellValue("Best %,Threshold");
+        HSSFCell cell2 = jumpRow.createCell(1);
+        cell2.setCellValue(maxClassification);
+        HSSFCell cell3 = jumpRow.createCell(2);
+        cell3.setCellValue(maxClassificationThreshold);
+
+        HSSFRow expRow = sheet.createRow(rownum+3);
+        HSSFCell expCell1 = expRow.createCell(0);
+        expCell1.setCellValue("TP");
+        HSSFCell expCell2 = expRow.createCell(1);
+        expCell2.setCellValue("FP");
+        HSSFCell expCell3 = expRow.createCell(2);
+        expCell3.setCellValue("FN");
+        HSSFCell expCell4 = expRow.createCell(3);
+        expCell4.setCellValue("TN");
+
+        HSSFRow lastRow = sheet.createRow(rownum+4);
+        HSSFCell lastCell1 = lastRow.createCell(0);
+        lastCell1.setCellValue(_bestConfusionMatrix.getTP());
+        HSSFCell lastCell2 = lastRow.createCell(1);
+        lastCell2.setCellValue(_bestConfusionMatrix.getFP());
+        HSSFCell lastCell3 = lastRow.createCell(2);
+        lastCell3.setCellValue(_bestConfusionMatrix.getFN());
+        HSSFCell lastCell4 = lastRow.createCell(3);
+        lastCell4.setCellValue(_bestConfusionMatrix.getTN());
+
 
     }
 
@@ -79,7 +107,7 @@ public class ROC_Curve {
     // Write the output to a file
         FileOutputStream fileOut = null;
         try {
-            fileOut = new FileOutputStream(_experimentName+".xls");
+            fileOut = new FileOutputStream(_path+".xls");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
