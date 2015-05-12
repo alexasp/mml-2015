72e08e08f055b0709d1a92920f26fb8ef8d283a3
Alexander Asplund <alexasplund@gmail.com>
2015-05-12 12:54:42 +0200

    Added script for preprocessing data, including 01-rescale and train-test split

--- python/australian.py
+++ python/australian.py
@@ -5,7 +5,7 @@ from sklearn import cross_validation
 samples = []
 labels = []
 
-with open('../data/australian_test_fixed.csv.csv') as csvdata:
+with open('../data/australian_test_fixed.csv') as csvdata:
 	for line in csvdata:
 		vector = line.split(',')
 		labels.append(int(vector[-1]))
