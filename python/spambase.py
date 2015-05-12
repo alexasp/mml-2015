fa1a1b7c39b23f8768d90db79b6b8249fdfb3420
Alexander Asplund <alexasplund@gmail.com>
2015-05-13 00:02:20 +0200

    Reran dataset genereation and new test

--- python/spambase.py
+++ python/spambase.py
@@ -1,4 +1,5 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 from sklearn import cross_validation
 
 
@@ -12,8 +13,19 @@ with open('../data/uci_spambase.csv.train') as csvdata:
 		sample = [float(feature) for feature in vector[:-1]]
 		samples.append(sample)
 
+testsamples = []
+testlabels = []
+with open('../data/uci_spambase.csv.test') as csvdata:
+	for line in csvdata:
+		vector = line.split(',')
+		testlabels.append(int(vector[-1]))
+		sample = [float(feature) for feature in vector[:-1]]
+		testsamples.append(sample)
+
 
 model = LogisticRegression()
+model.fit(samples, labels)
+print accuracy_score(model.predict(testsamples), testlabels)
 
 print cross_validation.cross_val_score(model, samples, labels)
 
