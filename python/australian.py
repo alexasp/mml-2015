48ad2bedbfa8c79ea8e5370994b178cb04db1dbd
Alexander Asplund <alexasplund@gmail.com>
2015-05-12 11:01:12 +0200

    australian logistic test

---
+++ python/australian.py
@@ -0,0 +1,20 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn import cross_validation
+
+
+samples = []
+labels = []
+
+with open('../data/australian_test_fixed.csv.csv') as csvdata:
+	for line in csvdata:
+		vector = line.split(',')
+		labels.append(int(vector[-1]))
+		sample = [float(feature) for feature in vector[:-1]]
+		samples.append(sample)
+
+
+model = LogisticRegression()
+
+print cross_validation.cross_val_score(model, samples, labels)
+
+
