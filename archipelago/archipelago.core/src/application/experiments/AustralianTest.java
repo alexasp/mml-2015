523cf7c9c7a557f08de1f91051f8250c39e8dbe8
Peter Froystad <p.froystad@gmail.com>
2015-06-01 16:28:14 +0200

    Added to the analysis chapter

--- archipelago/archipelago.core/src/application/experiments/AustralianTest.java
+++ archipelago/archipelago.core/src/application/experiments/AustralianTest.java
@@ -55,12 +55,12 @@ import static java.util.Collections.max;
                 testData = null;
             }
 
-            List<Integer> dataLimits = Arrays.asList(10, 20, 30, 40, 50, 60, 70, 80, 90, 100);
-            List<Integer> peerCounts = Arrays.asList(30);
+            List<Integer> dataLimits = Arrays.asList(100);
+            List<Integer> peerCounts = Arrays.asList(10);
 //        List<Integer> groupSizes = IntStream.range(2, 21).boxed().collect(Collectors.toList());
             List<Integer> groupSizes = Arrays.asList(5);
 //        List<PrivacyParam> privacyParams = IntStream.range(10, 11).mapToObj(i -> PrivacyParam.get(Math.pow(2, i), Math.pow(2, i))).collect(Collectors.toList());
-            List<Double> regularizations = IntStream.range(-2, -1).mapToDouble(i -> Math.pow(2, i)).boxed().collect(Collectors.toList());
+            List<Double> regularizations = IntStream.range(-4, 4).mapToDouble(i -> Math.pow(2, i)).boxed().collect(Collectors.toList());
             List<PrivacyParam> privacyParams = Arrays.asList(
                     new PrivacyParam(10.0)
             );
